f9efe6e73c610ac9c18728a66d53919f642199bc
VivyanKumar <82266868+VivyanKumar@users.noreply.github.com>
2021-05-12 16:38:53 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# CrumpleBall
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ sketch.js
@@ -0,0 +1,53 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ball;
+var bin;
+var ground;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1200, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	ball = new Ball(100,650,35);
+	ground = new Ground(600,700,1200,25);
+	bin1 = new Bin(1000,650,250,20);
+	bin2 = new Bin(875,600,20,100);
+	bin3 = new Bin(1125,600,20,100);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  ball.display();
+  ground.display();
+  bin1.display();
+  bin2.display();
+  bin3.display();
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+	if (keyCode === UP_ARROW){
+
+		Matter.Body.applyForce(ball.body,ball.body.position,{x:250,y:-250});
+	}
+}
